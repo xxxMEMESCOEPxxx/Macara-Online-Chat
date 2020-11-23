354fc3a850ca966ff4fa9e51e16a10b2c12a0858
xxxMEMESCOEPxxx <67339620+xxxMEMESCOEPxxx@users.noreply.github.com>
2020-11-23 15:30:55 -0500

    Add files via upload

---
+++ Client 1.1 Source/Form3.Designer.cs
@@ -0,0 +1,95 @@
+﻿
+namespace ChatClient
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form3));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(85, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(271, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Macara Online Chat Client (C)2020";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(56, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(337, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "(C) 2020 Andrew Maney All Right Reserved.";
+            // 
+            // button1
+            // 
+            this.button1.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.button1.Location = new System.Drawing.Point(156, 104);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(86, 25);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Ok";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(390, 141);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form3";
+            this.ShowInTaskbar = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show;
+            this.Text = "About";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ Client 1.1 Source/Form4.Designer.cs
@@ -0,0 +1,114 @@
+﻿
+namespace ChatClient
+{
+    partial class Form4
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.TreeNode treeNode1 = new System.Windows.Forms.TreeNode("How To Use");
+            System.Windows.Forms.TreeNode treeNode2 = new System.Windows.Forms.TreeNode("Room IP");
+            System.Windows.Forms.TreeNode treeNode3 = new System.Windows.Forms.TreeNode("Username");
+            System.Windows.Forms.TreeNode treeNode4 = new System.Windows.Forms.TreeNode("Previous Server");
+            System.Windows.Forms.TreeNode treeNode5 = new System.Windows.Forms.TreeNode("How to connect", new System.Windows.Forms.TreeNode[] {
+            treeNode2,
+            treeNode3,
+            treeNode4});
+            System.Windows.Forms.TreeNode treeNode6 = new System.Windows.Forms.TreeNode("How to disconnect");
+            System.Windows.Forms.TreeNode treeNode7 = new System.Windows.Forms.TreeNode("How to send a message");
+            System.Windows.Forms.TreeNode treeNode8 = new System.Windows.Forms.TreeNode("Help", new System.Windows.Forms.TreeNode[] {
+            treeNode1,
+            treeNode5,
+            treeNode6,
+            treeNode7});
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form4));
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(3, 13);
+            this.treeView1.Name = "treeView1";
+            treeNode1.Name = "Node1";
+            treeNode1.Tag = "use";
+            treeNode1.Text = "How To Use";
+            treeNode2.Name = "Node4";
+            treeNode2.Tag = "roomip";
+            treeNode2.Text = "Room IP";
+            treeNode3.Name = "Node5";
+            treeNode3.Tag = "username";
+            treeNode3.Text = "Username";
+            treeNode4.Name = "Node6";
+            treeNode4.Tag = "preserv";
+            treeNode4.Text = "Previous Server";
+            treeNode5.Name = "Node3";
+            treeNode5.Tag = "conn";
+            treeNode5.Text = "How to connect";
+            treeNode6.Name = "Node0";
+            treeNode6.Tag = "disscon";
+            treeNode6.Text = "How to disconnect";
+            treeNode7.Name = "Node1";
+            treeNode7.Tag = "msgsend";
+            treeNode7.Text = "How to send a message";
+            treeNode8.Name = "Node0";
+            treeNode8.Tag = "help";
+            treeNode8.Text = "Help";
+            this.treeView1.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+            treeNode8});
+            this.treeView1.Size = new System.Drawing.Size(287, 420);
+            this.treeView1.TabIndex = 0;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(306, 13);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(497, 420);
+            this.listBox1.TabIndex = 1;
+            // 
+            // Form4
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.treeView1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form4";
+            this.Text = "Help";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
